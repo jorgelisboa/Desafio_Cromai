aee78886e32fd6041cd734682b01a3f4051d1035
Jorge Lisboa <jorgelisboa659@gmail.com>
2021-08-06 15:05:41 -0300

    Esta é a versão pedida do app.

---
+++ app/src/main/java/com/example/cromai_desafio/TiposGraosCafe.java
@@ -0,0 +1,25 @@
+package com.example.cromai_desafio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class TiposGraosCafe extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipos_graos_cafe);
+        //Getting list in activity
+        ListView List = findViewById(R.id.listCafe);
+        //Getting extrar
+        Intent intent=getIntent();
+        String[] valueOfKey = intent.getStringArrayExtra("cafeArray");
+        //Show array
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, valueOfKey);//Set the adapter on my list
+        List.setAdapter(adapter);
+    }
+}
